2e1ffee4b87025bb75d798690f2c0b108dd74617
Mustafa6754 <144300820+Mustafa6754@users.noreply.github.com>
2023-09-07 15:59:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+// Chp1
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+
+// Chapter 2
+let myCamelCaseVariable = "This is a camelCase variable";
+var myVariable; // Declaration without definition
+myVariable = "This is a string value"; // Assignment with a string value
+let teamName = "My Awesome Team";
+alert("Our team name is: " + teamName);
+bestMan = "John";
+// chapter 3
+var caseQty; // You can also use "const" or "var" depending on your needs
+var caseQty = 144;
+var num = 9;
+var result;
+result = 5 + 3;
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+var myVariable = 5;
+myVariable = myVariable + 10;
+console.log(myVariable); // Output will be 15
+// Chapter 4
+var productCost = 3.45;
+var nameOfBand;
+var myNumber = 42;
+// chapter 5
+var remainder = 10 % 3; // remainder will be 1
+var largeNum = 1000 * 2000;
+var variable1 = 10; 
+var variable2 = 5; 
+var result = variable1 - variable2;
+// Prompt the user to enter the first number and convert it to a float
+var num1 = parseFloat(prompt("Enter the first number:"));
+
+// Prompt the user to enter the second number and convert it to a float
+var num2 = parseFloat(prompt("Enter the second number:"));
+
+// Calculate the result of the multiplication
+var result = num1 * num2;
+
+// Display the result in an alert
+alert("The result of the multiplication is: " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
